331756a4c1aac01a01b860ea09999b49f7205769
t <hectors.dev@gmail.com>
2020-04-28 06:05:34 -0400

    Updates

---
+++ precache-manifest.ebbc649a9ed71268f26ca61b5dba77bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042ff57048a41581e465f5d6b85a3fe3",
+    "url": "/verde-today/index.html"
+  },
+  {
+    "revision": "62e5a836c97b657945df",
+    "url": "/verde-today/static/css/main.06dbf1df.chunk.css"
+  },
+  {
+    "revision": "1140fb84a5c26adaa930",
+    "url": "/verde-today/static/js/2.3f358c18.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/verde-today/static/js/2.3f358c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62e5a836c97b657945df",
+    "url": "/verde-today/static/js/main.e9fd5719.chunk.js"
+  },
+  {
+    "revision": "c7517fe3f062d01cb4c3",
+    "url": "/verde-today/static/js/runtime-main.a6ec35f4.js"
+  },
+  {
+    "revision": "b0d4bb418c8346e987da66c1995bcedb",
+    "url": "/verde-today/static/media/dollarCroped.b0d4bb41.jpg"
+  },
+  {
+    "revision": "c0df966c9c61f0ab10f6ff564c702c40",
+    "url": "/verde-today/static/media/refresh-icon.c0df966c.svg"
+  }
+]);
